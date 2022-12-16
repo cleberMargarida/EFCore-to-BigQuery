f5311d8d55055d7d4ba6eabc61d74fed7bb40472
cleberMargarida <cleberfilho3@gmail.com>
2022-12-16 13:44:35 -0300

    update sample

--- ApiBQ/Controllers/TestController.cs
+++ ApiBQ/Controllers/TestController.cs
@@ -2,9 +2,11 @@
 using BigQuery.EntityFramework.Core;
 using BigQuery.EntityFramework.Core.Linq;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static BigQuery.EntityFramework.Core.BqFunc;
 
 namespace ApiBQ.Controllers
 {
@@ -21,12 +23,35 @@ namespace ApiBQ.Controllers
         [HttpGet]
         public async Task<IEnumerable<MyEntity>> GetAsync()
         {
-            MyEntity entity = new MyEntity {Id = 1, Name = "marvin", Update = new System.DateTime(2000, 1, 1) };
+            MyEntity entity = new MyEntity
+            {
+                Id = 1,
+                Name = "marvin",
+                Update = new DateTime(2021, 1, 1),
+                Test = new Test
+                {
+                    TestName = "teste name",
+                    TestId = 1,
+                    TestIntern = new TestIntern
+                    {
+                        Description = "cleber"
+                    }
+                }
+            };
             context.MyEntities.Add(entity);
+            await context.MyEntities.SaveAsync();
+
             entity.Update = System.DateTime.Now;
             context.MyEntities.Update(entity);
+            await context.MyEntities.SaveAsync();
+
             context.MyEntities.Remove(entity);
             await context.MyEntities.SaveAsync();
+
+            var query = context.MyEntities
+                .With(x => x.Where(x => x.Id > 0).Select(x => new { x.Name, x.Update }))
+                .Select(x => ToJsonString(x))
+                .ToList();
 
             return await context.MyEntities.ToListAsync(); 
         }
--- ApiBQ/MyEntity.cs
+++ ApiBQ/MyEntity.cs
@@ -6,10 +6,28 @@ namespace ApiBQ
     [Table("`data.myentity`")]
     public class MyEntity
     {
-        [Key]
+        [Key, ColumnName("id")]
         public int Id { get; set; }
+
+        [ColumnName("name")]
         public string Name { get; set; }
+
+        [ColumnName("update")]
         public DateTime Update { get; set; }
+
+        public Test Test { get; set; }
+    }
+
+    public class Test
+    {
+        public string TestName { get; set; }
+        public int TestId { get; set; }
+        public TestIntern TestIntern { get; set; }
+    }
+
+    public class TestIntern
+    {
+        public string Description { get; set; }
     }
 
 }
